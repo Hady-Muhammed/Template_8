eb1407f5f44e73b2684300a8a4d8f3d93067105c
Hady_Muhammed <hadyymohammed@gmail.com>
2022-07-24 23:25:45 +0300

    Website Uploaded

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 let nav = document.querySelector("nav");
 let header = document.querySelector("header");
 
-header.style.zIndex = "3 !important";
+header.style.zIndex = "3";
 
 window.onscroll = function () {
-    if (window.scrollY >= 150) 
-        nav.style.backgroundColor = "#181a23";
-    else 
-        nav.style.backgroundColor = "";
-}
+  if (window.scrollY >= 150) nav.style.backgroundColor = "#181a23";
+  else nav.style.backgroundColor = "";
+};
